0dbde93af1e2496262b28da442e7d20f8fa37fa5
Mohammed <mohammed.abdelnaby@georgebrown.ca>
2021-10-05 13:17:11 -0700

    finished unorderedarray class, now starting orderedarray class

--- Game2001_A1_AbdelnabyMohammed/Main.cpp
+++ Game2001_A1_AbdelnabyMohammed/Main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include "UnorderedArray.h"
 using namespace std;
 
 
 int main()
 {
+	UnorderedArry<int> array(3);
+	array.push(3);
+	array.push(53);
+	array.push(83);
+	array.push(23);
+	array.push(82);
 
+	array[2] = 112;
+	array.pop();
+	array.remove(2);
+
+	for (int i = 0; i < array.getSize(); i++)
+	{
+		cout << array[i] << endl;
+	}
+
+	cout << array.search(53) << endl;
 	return 0;
 }
--- Game2001_A1_AbdelnabyMohammed/UnorderedArray.h
+++ Game2001_A1_AbdelnabyMohammed/UnorderedArray.h
@@ -5,14 +5,113 @@ template<class T>
 class UnorderedArry
 {
 public:
-	UnorderedArry(int size, int growBy = 1) : m_arry(NULL), m_maxSize(0), m_growSize(0), m_numElements(0)
+	UnorderedArry(int size, int growBy = 1) : m_array(NULL), m_maxSize(0), m_growSize(0), m_numElements(0)
 	{
 		if (size >= 0)
 		{
 			m_maxSize = size;
 			m_array = new T[m_maxSize];
-			memset(m_array, 0, m_maxSize);
+			memset(m_array, 0, sizeof(T) * m_maxSize);
+
+			m_growSize = ((growBy > 0)) ? growBy : 0;
 		}
+	}
+	~UnorderedArry()
+	{
+		if (m_array != nullptr)
+		{
+			delete[] m_array;
+			m_array = nullptr;
+		}
+	}
+	void push(T val)
+	{
+		assert(m_array != nullptr);
+		if (m_numElements >= m_maxSize)
+		{
+			Expand();
+		}
+		m_array[m_numElements] = val;
+		m_numElements++;
+	}
+	void pop()
+	{
+		if (m_numElements > 0)
+		{
+			m_numElements--;
+		}
+	}
+	void remove(int index)
+	{
+		assert(m_array != nullptr);
+		if (index >= m_numElements)
+		{
+
+			return;
+		}
+		for (int i = index; i < m_numElements; i++)
+		{
+			if (i + 1 < m_numElements)
+			{
+				m_array[i] = m_array[i + 1];
+			}
+		}
+		m_numElements--;
+	}
+	int search(T val)
+	{
+		assert(m_array != nullptr);
+		for (int i = 0; i < m_numElements; i++)
+		{
+			if (m_array[i] == val)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	T& operator[](int index)
+	{
+		assert(m_array != nullptr && index < m_numElements);
+		return m_array[index];
+	}
+	void clear()
+	{
+		m_numElements = 0;
+	}
+	int getSize()
+	{
+		return m_numElements;
+	}
+	int getMaxSize()
+	{
+		return m_maxSize;
+	}
+	int getGrowSize()
+	{
+		return m_growSize;
+	}
+	void setGrowSize(int val)
+	{
+		assert(val >= 0);
+		m_growSize = val;
+	}
+private:
+	bool Expand()
+	{
+		if (m_growSize <= 0)
+		{
+			return false;
+		}
+		T* temp = new T[m_maxSize + m_growSize];
+		assert(temp != nullptr);
+		memcpy(temp, m_array, sizeof(T) * m_maxSize);
+
+		delete[] m_array;
+		m_array = temp;
+		temp = nullptr;
+		m_maxSize += m_growSize;
+		return true;
 	}
 private:
 	T* m_array;
